4e8dd1dfb4a2c025d664c7e0231b1932068d14aa
Dong Darong <148829792+DongDarong@users.noreply.github.com>
2024-09-10 12:53:29 +0700

    update database

--- lab-website/database.sql
+++ lab-website/database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE request_lab;
 CREATE TABLE users (
-    ID INT PRIMARY KEY NOT NULL ,
+    ID INT PRIMARY KEY AUTO INCREMENT,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     gender VARCHAR(10),
@@ -12,10 +12,12 @@ CREATE TABLE users (
     password VARCHAR(255),
     date_time DATETIME,
     phone VARCHAR(255),
+    position VARCHAR(20),
+    address VARCHAR(100)
 );
 
 CREATE TABLE Feedback (
-    ID INT PRIMARY KEY NOT NULL ,
+    ID INT PRIMARY KEY AUTO INCREMENT ,
     user_id INT,
     comments TEXT,
     date_time DATETIME,
@@ -23,27 +25,27 @@ CREATE TABLE Feedback (
 );
 
 CREATE TABLE Codes (
-    ID INT PRIMARY KEY NOT NULL ,
-    email VARCHAR(100) UNIQUE,
-    code VARCHAR(100) UNIQUE,
-    expire DATETIME UNIQUE
+    ID INT PRIMARY KEY AUTO INCREMENT ,
+    email VARCHAR(100) INDEX,
+    code VARCHAR(100) INDEX,
+    expire DATETIME INDEX
 );
 
 CREATE TABLE Lab (
-    ID INT PRIMARY KEY NOT NULL ,
+    ID INT PRIMARY KEY AUTO INCREMENT ,
     name_lab VARCHAR(100),
     time TIME
 );
 
 CREATE TABLE Session (
-    ID INT PRIMARY KEY NOT NULL ,
+    ID INT PRIMARY KEY AUTO INCREMENT ,
     sessions VARCHAR(100),
     number_Sessions INT,
     time TIME
 );
 
 CREATE TABLE Information (
-    ID INT PRIMARY KEY NOT NULL ,
+    ID INT PRIMARY KEY AUTO INCREMENT ,
     user_id INT,
     date DATE,
     lab_id INT,
